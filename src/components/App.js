b8a279a49af750b0c99ede5e3b646bce32c8c38f
Cedric <cedricj765@mail.com>
2023-09-15 00:52:05 -0500

    completed lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,46 @@
-// create your App component here
+// import React, { useEffect, useState } from 'react'
+
+// function App () {
+
+//     const [dogPic, setDogPic] = useState(null)
+
+    
+
+//     useEffect( () => {
+//         fetch('https://dog.ceo/api/breeds/image/random')
+//             .then(res => res.json())
+//             .then(data => {
+//                 setDogPic(data.message)
+//             })
+//         }, [])
+
+//         if (!dogPic) return <p>Loading...</p>;
+
+//         return  <img src={dogPic} alt='A random dog' />;
+    
+
+  
+// }
+
+// export default App
+
+import React, { useState, useEffect } from "react";
+
+function App() {
+  const [dogPic, setDogPic] = useState(null);
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        setDogPic(data.message);
+      });
+  }, []);
+  // use an empty dependencies array, so we only run the fetch request ONCE
+
+  if (!dogPic) return <p>Loading...</p>;
+
+  return <img src={dogPic} alt="A Random Dog" />;
+}
+
+export default App;
